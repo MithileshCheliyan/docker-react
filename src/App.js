9a08972f135af868889cd48550770057e9599e7e
Cheliyan, Mithilesh <Mithilesh.Cheliyan@ellucian.com>
2021-06-06 14:47:29 +0530

    hh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Happieee Weekend Shrruuu !!!
+          Welcome World !!!
         </p>
         <a
           className="App-link"
